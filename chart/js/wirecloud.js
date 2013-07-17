fb8ab5a7bdbf99cc1cf4f081041e19ae3b197820
Manuel Warum <manuel.warum.fl@ait.ac.at>
2013-07-17 11:49:42 +0200

    Resilient integration with MashupPlatform API (chart widget)

---
+++ chart/js/wirecloud.js
@@ -0,0 +1,39 @@
+/*
+ * Iff the Wirecloud environment is available, register endpoints.
+ */
+$(function () {
+    if (typeof MashupPlatform === 'undefined') {
+        console.warn('Wirecloud environment not detected.');
+    } else {
+        var applyPreferences = function () {
+            series.setTitle(MashupPlatform.prefs.get('title'));
+        };
+
+        MashupPlatform.prefs.registerCallback(applyPreferences);
+        applyPreferences();
+
+        MashupPlatform.wiring.registerCallback('series_data_1d', function (data) {
+            series.setData(parseJSON(to2d(data)));
+        });
+
+        MashupPlatform.wiring.registerCallback('series_data_2d', function (data) {
+            series.setData(parseJSON(data));
+        });
+    }
+
+    function to2d(array) {
+        var matrix = new [];
+        for (var i = 0; i < array.length; i++)
+            matrix[i] = [ array[i] ];
+        return matrix;
+    }
+
+    function parseJSON(data) {
+        if (typeof data === 'string') {
+            data = $.parseJSON(data);
+            if (typeof data !== 'array')
+                console.warn('Provided JSON data is not an array');
+        }
+        return data;
+    }
+});
