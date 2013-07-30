de4e4348060c39c69376f0aece59d923c3d99c22
Manuel Warum <manuel.warum.fl@ait.ac.at>
2013-07-30 12:21:14 +0200

    Working on Wirecloud integration for OOI viewer

--- ooi_viewer/js/wirecloud.js
+++ ooi_viewer/js/wirecloud.js
@@ -14,6 +14,19 @@ $(function () {
             // do something with the identifier now; eg. fetch it from the external resource, or something like that
             console.debug('Implement me :(');
         });
+
+        $('#btn-locate').click(function() {
+            console.debug('Implement me :(');
+
+            // the following lines might throw errors as parts of the implementation are still missing
+            if (window.hasOwnProperty('currentObj') && window.currentObj) {
+                MashupPlatform.wiring.pushEvent('locate-ooi', window.currentObj.id);
+
+                if (window.currentObj.hasOwnProperty('lon') && window.currentObj.hasOwnProperty('lat')
+                    && window.currentObj.lon && window.currentObj.lat)
+                    MashupPlatform.wiring.pushEvent('locate-lon-lat', JSON.stringify({ lon: window.currentObj.lon, lat: window.currentObj.lat }));
+            }
+        });
     }
 
     function parseJSON(data) {
