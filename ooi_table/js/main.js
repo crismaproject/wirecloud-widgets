ab4a8fa11c0e74313d86e4daa61f49dd2919d82f
Manuel Warum <manuel.warum.fl@ait.ac.at>
2013-09-13 11:51:16 +0200

    OOI table is more dynamic now

--- ooi_table/js/main.js
+++ ooi_table/js/main.js
@@ -1,10 +1,25 @@
 window.ooi_wsr_uri = 'http://localhost/api';
 
-function OOITable(container) {
+function OOITable(container, columns) {
     /** @private */
     this.container = container;
 
-    this.onSelected = function (row) {};
+    /** @private */
+    this.columns = [ ];
+
+    /**
+     * @param selected an array of selected rows
+     */
+    this.onSelectionChanged = null;
+
+    if (!$('tbody', container).length) $(container).prepend($('<tbody></tbody>'));
+    if (!$('thead', container).length) $(container).prepend($('<thead></thead>'));
+    if (!$('thead tr', container).length) $('thead', container).append($('<tr></tr>'));
+
+    for (var key in columns) {
+        $('thead tr', container).append($('<th></th>').text(key));
+        this.columns.push(columns[key]);
+    }
 }
 
 /**
@@ -13,37 +28,57 @@ function OOITable(container) {
  * @param {string} row.id the OOI's unique identifier
  * @param {string} row.type the OOI's type
  * @param {string} row.site the OOI's current location
- * @param {object?} opt additional data that gets attached to the row's markup
  */
-OOITable.prototype.addRow = function(row, opt) {
+OOITable.prototype.addRow = function(row) {
     var me = this;
-    var row = $('<tr></tr>')
+    var tr = $('<tr></tr>')
         .attr('data-row-id', row.id)
-        .append($('<th></th>').text(row.id))
-        .append($('<td></td>').text(row.type || '?'))
-        .append($('<td></td>').text(row.site || '?'))
         .click(function () {
-            $('tr.selected').removeClass('selected');
-            $(this).addClass('selected');
-            if (me.onSelected)
-                me.onSelected(opt);
+            if ($(this).is('.selected'))
+                $(this).removeClass('selected');
+            else
+                $(this).addClass('selected');
+            me.updateSelectionCount();
+            if (me.onSelectionChanged)
+                me.onSelectionChanged(me.getSelection());
         });
-    if (opt) row.attr('data-orig', JSON.stringify(opt));
-    $('tbody', this.container).append(row);
+
+    for (var i = 0; i < this.columns.length; i++)
+        tr.append($('<td></td>').text(row.hasOwnProperty(this.columns[i]) ? row[this.columns[i]] : '?'));
+
+    tr.attr('data-orig', JSON.stringify(row));
+    $('tbody', this.container).append(tr);
 }
 
 OOITable.prototype.clear = function() {
     $('tbody', this.container).empty();
+    this.updateSelectionCount();
 }
 
 OOITable.prototype.unselectAll = function() {
     $('tr.selected', this.container).removeClass('selected');
+    this.updateSelectionCount();
 }
 
 OOITable.prototype.selectAll = function() {
-    $('tr').not('.selected').addClass('selected');
+    $('tr', this.container).not('.selected').addClass('selected');
+    this.updateSelectionCount();
 }
 
 OOITable.prototype.select = function(id) {
-    $('tr[data-row-id="'+ id +'"]').not('.selected').addClass('selected');
+    $('tr[data-row-id="'+ id +'"]', this.container).not('.selected').addClass('selected');
+    this.updateSelectionCount();
+}
+
+OOITable.prototype.updateSelectionCount = function () {
+    $('.selection-count').text($('tr[data-row-id].selected').length);
+}
+
+OOITable.prototype.getSelection = function () {
+    var selection = [];
+    $('tr.selected[data-orig]', this.container).each(function (index, value) {
+        var ooi = JSON.parse($(value).attr('data-orig'));
+        selection.push(ooi);
+    });
+    return selection;
 }
--- ooi_table/js/wirecloud.js
+++ ooi_table/js/wirecloud.js
@@ -2,27 +2,16 @@
  * Iff the Wirecloud environment is available, register endpoints.
  */
 $(function () {
-    table.onSelected = function (row) {
-        var selection = [];
-        $('#ooi-table tr.selected').each(function (index, value) {
-            var ooi = JSON.parse($(value).attr('data-orig'));
-            selection.push(ooi);
-        });
-        MashupPlatform.wiring.pushEvent('oois_selected_out', JSON.stringify(selection));
+    table.onSelectionChanged = function (selected) {
+        MashupPlatform.wiring.pushEvent('oois_selected_out', JSON.stringify(selected));
     };
 
     MashupPlatform.wiring.registerCallback('oois_in', function (data) {
         var oois = JSON.parse(data);
 
         table.clear();
-        for (var i = 0; i < oois.length; i++) {
-            var ooi = oois[i];
-            table.addRow({
-                id: ooi.id,
-                type: ooi.type,
-                site: ooi.site
-            }, ooi);
-        }
+        for (var i = 0; i < oois.length; i++)
+            table.addRow(oois[i]);
     });
 
     MashupPlatform.wiring.registerCallback('oois_selected_in', function (data) {
