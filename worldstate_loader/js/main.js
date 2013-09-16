d4b213f5274d254020ff03f2ebe8155acb0b7fd8
Manuel Warum <manuel.warum.ait@gmail.com>
2013-09-16 12:36:24 +0200

    Worldstate Loader now loads OOIs

--- worldstate_loader/js/main.js
+++ worldstate_loader/js/main.js
@@ -1,13 +1,32 @@
 var apiUri = null;
+var enforcedWorldstate = null;
 var applyPreferences = function () {
     apiUri = MashupPlatform.prefs.get('api');
+    var enforcedWorldstatePref = MashupPlatform.prefs.get('force-ws');
+
+    if (enforcedWorldstatePref && enforcedWorldstate !== enforcedWorldstatePref) {
+        enforcedWorldstate = enforcedWorldstatePref;
+        loadWorldstate(enforcedWorldstate);
+    }
 };
 
 MashupPlatform.prefs.registerCallback(applyPreferences);
 applyPreferences();
 
-MashupPlatform.wiring.registerCallback('simulation_id', function (simulationId) {
-    if (!apiUri || !simulationId) return;
+MashupPlatform.wiring.registerCallback('simulation_id', loadLastWorldstateForSimulation);
+
+function loadWorldstate(worldstateId) {
+    MashupPlatform.http.makeRequest(apiUri + '/WorldState/' + worldstateId, {
+        method: 'GET',
+        requestHeaders: { 'Accept': 'application/json' },
+        onSuccess: function (response) { MashupPlatform.wiring.pushEvent('worldstate', response.responseText); },
+        onFailure: function () { alert('Request to OOI-WSR failed!'); }
+    });
+    loadOOIs(worldstateId);
+}
+
+function loadLastWorldstateForSimulation (simulationId) {
+    if (enforcedWorldstate || !apiUri || !simulationId) return;
 
     // simulation ID known, load the appropriate world states from the OOI-WSR
     var wsUri = apiUri + '/WorldState?simulationId=' + simulationId; // TODO: filter by simulation id on remote
@@ -15,7 +34,7 @@ MashupPlatform.wiring.registerCallback('simulation_id', function (simulationId) 
         method: 'GET',
         requestHeaders: { 'Accept': 'application/json' },
         onSuccess: function (response) {
-            var jsonResponse = JSON.parse(response);
+            var jsonResponse = JSON.parse(response.responseText);
             var lastWorldState = null;
 
             for (var i = 0; i < jsonResponse.length; i++) {
@@ -25,8 +44,24 @@ MashupPlatform.wiring.registerCallback('simulation_id', function (simulationId) 
 
             if (lastWorldState) { // we have a working latest world state, propagate
                 MashupPlatform.wiring.pushEvent('worldstate', JSON.stringify(lastWorldState));
+                loadOOIs(lastWorldState.worldStateId);
             }
         },
         onFailure: function () { alert('Request to OOI-WSR failed!'); }
     });
-});
+}
+
+/**
+ * @param {integer} worldstateId
+ */
+function loadOOIs (worldstateId) {
+    var ooiUri = apiUri + '/Entity?wsid=' + worldstateId;
+    MashupPlatform.http.makeRequest(ooiUri, {
+        method: 'GET',
+        requestHeaders: { 'Accept': 'application/json' },
+        onSuccess: function (response) {
+            MashupPlatform.wiring.pushEvent('oois', response.responseText);
+        },
+        onFailure: function () { alert('Request to OOI-WSR failed!'); }
+    });
+}
