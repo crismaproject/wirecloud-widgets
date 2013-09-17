17f0ef57ba94f9bf0df075344e89f519af8c3554
Manuel Warum <manuel.warum.ait@gmail.com>
2013-09-17 14:18:10 +0200

    Rewrite of OOI viewer table (non-functional right now)

--- ooi_viewer/js/main.js
+++ ooi_viewer/js/main.js
@@ -1,107 +1,19 @@
-/**
- * Creates a new OOI Viewer instance.
- * @param {string} container a CSS selector declaring the container to use (should be - or contain - a &lt;table&gt;)
- * @constructor
- */
-function OOIViewer(container) {
-    this.container = container;
+var types = { };
 
-    this.actionCallbacks = {};
+function createTableFor(entries) {
+    var parent = $('<div></div>').addClass('grp');
+    for (var i = 0; i < entries.length; i++)
+        parent.append($('<div></div>')
+            .addClass('row')
+            .append($('<div></div>')
+                .addClass('col-md-3')
+                .text(entries[i].key))
+            .append($('<div></div>')
+                .addClass('col-md-9')
+                .text(entries[i].value)));
+    return parent;
 }
 
-/**
- * Sets the table's contents.
- * @param {string} identifier the OOI's identifier.
- * @param {Object} obj an object where keys are used as row headers and values as, well, values.
- * @example
- * ooiViewer.set(123, {'Name': 'Alice', 'Location': 'Wonderland'}) // shows an OOI with id 123, the name Alice and the location Wonderland.
- */
-OOIViewer.prototype.set = function (identifier, obj) {
-    var tbody = $(this.container).find('tbody');
-    tbody.find('.gen').remove();
-    tbody.find('.ooi-id').text(identifier);
-
-    for (var key in obj) {
-        var value = obj[key] != null ? obj[key].toString() : '';
-        var row = $('<tr></tr>').addClass('gen')
-            .append($('<th></th>').text(key))
-            .append($('<td></td>').text(value));
-        tbody.append(row);
-    }
-};
-
-/**
- * Creates a button in the UI that triggers a JavaScript function if it is clicked.
- * @param label The label that will be shown on the generated button.
- * @param callback a method that should be called when the generated button is clicked.
- * @returns {String} Returns a unique identifier that should be used for the removeAction method.
- */
-OOIViewer.prototype.addAction = function (label, callback) {
-    this.removeAction.call(this, label);
-    var uid = generateIdentifier();
-    this.actionCallbacks[uid] = callback;
-    $(this.container).find('.actions').append(
-        $('<button></button>')
-            .addClass('btn')
-            .addClass('a_' + uid.toUpperCase())
-            .text(label)
-            .click(callback)
-    );
-    return uid;
-};
-
-/**
- * Removes a previously registered button from the UI.
- * @param uid the unique identifier that was generated when the action was registered with addAction.
- */
-OOIViewer.prototype.removeAction = function (uid) {
-    if (this.actionCallbacks.hasOwnProperty(uid)) {
-        $(this.container).find('.actions button.a_' + uid.toUpperCase()).remove();
-        delete this.actionCallbacks[uid];
-    }
-};
-
-/**
- * Generates an RFC 4122 v4 compliant unique identifier.
- * (Kindly borrowed from http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript)
- * @returns {string} a 32 characters long unique identifier with separators.
- */
-function generateIdentifier() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-        return v.toString(16);
-    });
-}
-
-window.ooi_wsr_uri = 'http://localhost/api';
-var worldstateId = null;
-var entityId = null;
-var entityTypes = {};
-
-function attemptUpdate() {
-    if (worldstateId && entityId) {
-        $.get(window.ooi_wsr_uri + '/Entity/?wsid=' + worldstateId, function (response) {
-            var entity = null;
-            for (var i = 0; i < response.length && !entity; i++)
-                if (response[i].entityId == entityId)
-                    entity = response[i];
-            if (!entity || typeof entityTypes[entity.entityTypeId] === 'undefined') return;
-
-            var entityType = entityTypes[entity.entityTypeId];
-            var entityProperties = {
-                'Name': entity.entityName || '',
-                'Description': entity.entityDescription || '',
-                'Type': entityType.name,
-                'Type description': entityType.description || ''
-            };
-            for (var i = 0; i < entity.entityInstancesProperties.length; i++) {
-                var current = entity.entityInstancesProperties[i];
-                var displayKey = entityType.properties[current.entityTypePropertyId].name;
-                var displayValue = current.entityPropertyValue;
-                entityProperties[displayKey] = displayValue;
-            }
-
-            ooiViewer.set(entityId, entityProperties);
-        }, 'json');
-    }
+function addEntries(entries) {
+    $('#container').append(createTableFor(entries));
 }
--- ooi_viewer/js/wirecloud.js
+++ ooi_viewer/js/wirecloud.js
@@ -4,40 +4,17 @@
 $(function () {
     if (typeof MashupPlatform === 'undefined') {
         console.warn('Wirecloud environment not detected.');
-    } else if (typeof ooiViewer === 'undefined') {
-        console.warn('"ooiViewer" variable is not defined.')
     } else {
-        MashupPlatform.wiring.registerCallback('worldstate', function (data) {
-            worldstateId = data;
-            attemptUpdate();
+        MashupPlatform.wiring.registerCallback('oois', function (data) {
         });
 
-        MashupPlatform.wiring.registerCallback('entity', function (data) {
-            entityId = data;
-            attemptUpdate();
+        MashupPlatform.wiring.registerCallback('types', function (data) {
+            var ooiTypes = JSON.parse(data);
+            types = { };
+            for (var i = 0; i < ooiTypes.length; i++) {
+                var ooiType = ooiTypes[i];
+                types[ooiType.entityTypeId] = ooiType;
+            }
         });
     }
-
-    $.get(window.ooi_wsr_uri + '/EntityType', function (entityData) {
-        for (var i = 0; i < entityData.length; i++) {
-            var cur = entityData[i];
-            entityTypes[cur.entityTypeId] = {
-                name: entityData[i].entityTypeName.trim(),
-                description: entityData[i].entityTypeDescription.trim(),
-                properties: {}
-            };
-        }
-
-        $.get(window.ooi_wsr_uri + '/EntityTypeProperty', function (propertyData) {
-            for (var i = 0; i < propertyData.length; i++) {
-                var cur = propertyData[i];
-                var entityId = cur.entityTypeId;
-                if (typeof entityTypes[entityId] !== 'undefined')
-                    entityTypes[entityId].properties[cur.entityTypePropertyId] = {
-                        name: cur.entityTypePropertyName.trim(),
-                        description: cur.entityTypePropertyDescription.trim()
-                    };
-            }
-        }, 'json');
-    }, 'json');
 });
