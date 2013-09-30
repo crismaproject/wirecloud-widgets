517020a2a875d5335b6847a2265d518ff9ee8e30
Manuel Warum <manuel.warum.fl@ait.ac.at>
2013-09-30 12:17:46 +0200

    OOI table: cleaning up wirecloud code

--- ooi_table/js/wirecloud.js
+++ ooi_table/js/wirecloud.js
@@ -3,26 +3,22 @@
 /*
  * Iff the Wirecloud environment is available, register endpoints.
  */
-$(function () {
-    var propagate = true;
-
-    $(document).on('selectionChanged', function () { console.log('selection changed'); });
-
-    if (typeof MashupPlatform !== 'undefined') {
+if (typeof MashupPlatform !== 'undefined') {
+    $(function () {
         $(document).on('selectionChanged', function () {
-            MashupPlatform.wiring.pushEvent('oois_selected_out', JSON.stringify(selected));
+            MashupPlatform.wiring.pushEvent('oois_selected_out', JSON.stringify(getSelection()));
         });
+    });
 
-        MashupPlatform.wiring.registerCallback('oois_in', function (data) {
-            groupManager.setOOIs(JSON.parse(data));
-        });
+    MashupPlatform.wiring.registerCallback('oois_in', function (data) {
+        groupManager.setOOIs(JSON.parse(data));
+    });
 
-        MashupPlatform.wiring.registerCallback('oois_selected_in', function (data) {
-            groupManager.setSelected(JSON.parse(data));
-        });
+    MashupPlatform.wiring.registerCallback('oois_selected_in', function (data) {
+        groupManager.setSelected(JSON.parse(data));
+    });
 
-        MashupPlatform.wiring.registerCallback('types', function (data) {
-            groupManager.setOOITypes(JSON.parse(data));
-        });
-    }
-});
+    MashupPlatform.wiring.registerCallback('types', function (data) {
+        groupManager.setOOITypes(JSON.parse(data));
+    });
+}
