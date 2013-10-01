868c2f138190c5c244c9e063d23c2bdedab533cb
Manuel Warum <manuel.warum.fl@ait.ac.at>
2013-10-01 12:21:47 +0200

    WS loader: bumping to 0.1.4.1
    WS loader: small refactoring and code cleanup

--- worldstate_loader/js/main.js
+++ worldstate_loader/js/main.js
@@ -11,15 +11,9 @@ var applyPreferences = function () {
         loadWorldstate(enforcedWorldstate);
     }
 
-    var typeUri = apiUri + '/EntityType';
-    MashupPlatform.http.makeRequest(typeUri, {
-        method: 'GET',
-        requestHeaders: { 'Accept': 'application/json' },
-        onSuccess: function (response) {
-            MashupPlatform.wiring.pushEvent('ooi-types', response.responseText);
-        },
-        onFailure: function () { alert('Request to OOI-WSR failed! (while trying to load entity types)'); }
-    });
+    apiCall('EntityType', function (response) {
+        MashupPlatform.wiring.pushEvent('ooi-types', response.responseText);
+    }, 'Request to OOI-WSR failed! (while trying to load entity types)');
 };
 
 MashupPlatform.prefs.registerCallback(applyPreferences);
@@ -28,54 +22,68 @@ applyPreferences();
 MashupPlatform.wiring.registerCallback('simulation', loadLastWorldstateForSimulation);
 
 function loadWorldstate(worldstateId) {
-    MashupPlatform.http.makeRequest(apiUri + '/WorldState/' + worldstateId, {
-        method: 'GET',
-        requestHeaders: { 'Accept': 'application/json' },
-        onSuccess: function (response) { MashupPlatform.wiring.pushEvent('worldstate', response.responseText); },
-        onFailure: function () { alert('Request to OOI-WSR failed! (during loadWorldstate)'); }
-    });
+    apiCall('WorldState/' + worldstateId, function (response) {
+        MashupPlatform.wiring.pushEvent('worldstate', response.responseText);
+    }, 'Request to OOI-WSR failed! (during loadWorldstate)');
     loadOOIs(worldstateId);
 }
 
-function loadLastWorldstateForSimulation (simulationData) {
+function loadLastWorldstateForSimulation(simulationData) {
     if (enforcedWorldstate || !apiUri || !simulationData) return;
 
     var simulation = JSON.parse(simulationData);
     if (!simulation || !simulation.hasOwnProperty('simulationId')) throw 'Provided item does not have a "simulationId" property!';
 
-    var wsUri = apiUri + '/WorldState?simulationId=' + simulation.simulationId; // TODO: filter by simulation id on remote
-    MashupPlatform.http.makeRequest(wsUri, {
-        method: 'GET',
-        requestHeaders: { 'Accept': 'application/json' },
-        onSuccess: function (response) {
-            var jsonResponse = JSON.parse(response.responseText);
-            var lastWorldState = null;
+    apiCall('WorldState', function (response) { // TODO: filter by simulation id on remote
+        var jsonResponse = JSON.parse(response.responseText);
+        var lastWorldState = null;
 
-            for (var i = 0; i < jsonResponse.length; i++) {
-                if (!jsonResponse[i].hasOwnProperty('simulationId') || jsonResponse[i].simulationId != simulationData) continue;
-                lastWorldState = jsonResponse[i];
-            }
+        for (var i = 0; i < jsonResponse.length; i++) {
+            if (!jsonResponse[i].hasOwnProperty('simulationId') || jsonResponse[i].simulationId != simulationData) continue;
+            lastWorldState = jsonResponse[i];
+        }
 
-            if (lastWorldState) { // we have a working latest world state, propagate
-                MashupPlatform.wiring.pushEvent('worldstate', JSON.stringify(lastWorldState));
-                loadOOIs(lastWorldState.worldStateId);
-            }
-        },
-        onFailure: function () { alert('Request to OOI-WSR failed! (during loadLastWorldstateForSimulation)'); }
-    });
+        if (lastWorldState) { // we have a working latest world state, propagate
+            MashupPlatform.wiring.pushEvent('worldstate', JSON.stringify(lastWorldState));
+            loadOOIs(lastWorldState.worldStateId);
+        }
+    }, 'Request to OOI-WSR failed! (during loadLastWorldstateForSimulation)', { 'simulationId': simulation.simulationId });
 }
 
 /**
  * @param {int} worldstateId
  */
-function loadOOIs (worldstateId) {
-    var ooiUri = apiUri + '/Entity?wsid=' + worldstateId;
-    MashupPlatform.http.makeRequest(ooiUri, {
+function loadOOIs(worldstateId) {
+    apiCall('Entity', function (response) {
+        MashupPlatform.wiring.pushEvent('oois', response.responseText);
+    }, 'Request to OOI-WSR failed! (during loadOOIs)', { 'wsid': worldstateId });
+}
+
+/**
+ * Performs a GET request to the OOI-WSR REST API and expects a JSON-encoded response.
+ * @param {string} path the (relative) path/route to request; if any query parameters are required for this request,
+ * either put them here, or declare them in arguments, but not both.
+ * @param {function} onSuccess a callback to handle the response.
+ * @param {string?} failureMessage a message that will be sent to the user via window.alert iff the request failed.
+ * @param {object?} arguments any query parameters that should be sent along with the request; if any query parameters
+ * are required for this request, either put them here, or declare them in the path, but not both.
+ */
+function apiCall(path, onSuccess, failureMessage, arguments) {
+    var uri = apiUri + '/' + path;
+    if (typeof arguments !== 'undefined' && arguments !== {}) {
+        var queryString = '?';
+        for (var key in arguments) {
+            if (queryString.length > 1) queryString += '&';
+            queryString += encodeURIComponent(key) + '=' + encodeURIComponent(arguments[key]);
+        }
+        uri += queryString;
+    }
+    MashupPlatform.http.makeRequest(uri, {
         method: 'GET',
         requestHeaders: { 'Accept': 'application/json' },
-        onSuccess: function (response) {
-            MashupPlatform.wiring.pushEvent('oois', response.responseText);
-        },
-        onFailure: function () { alert('Request to OOI-WSR failed! (during loadOOIs)'); }
+        onSuccess: onSuccess,
+        onFailure: function () {
+            alert(failureMessage || 'Request to ' + uri + ' failed.');
+        }
     });
 }
