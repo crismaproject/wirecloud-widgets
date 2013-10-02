8e91be731ae7fef71230fef5667df7fee9b6dda0
Manuel Warum <manuel.warum.fl@ait.ac.at>
2013-10-02 09:37:25 +0200

    ooi_command binds to wirecloud earlier

--- ooi_command/js/wirecloud.js
+++ ooi_command/js/wirecloud.js
@@ -3,27 +3,31 @@
 /*
  * Iff the Wirecloud environment is available, register endpoints.
  */
-$(function () {
-    if (typeof MashupPlatform === 'undefined') {
-        console.warn('Wirecloud environment not detected.');
-        var logFunction = function(event, data) { console.log(data); };
+if (typeof MashupPlatform === 'undefined') {
+    console.warn('Wirecloud environment not detected.');
 
+    $(function () {
+        var logFunction = function (event, data) {
+            console.log(data);
+        };
         $('body')
             .on('command', logFunction)
             .on('areaCreated', logFunction);
-    } else {
-        MashupPlatform.wiring.registerCallback('oois_in', function (data) {
-            setObjectsOfInterest(JSON.parse(data));
-        });
+    });
+} else {
+    MashupPlatform.wiring.registerCallback('oois_in', function (data) {
+        setObjectsOfInterest(JSON.parse(data));
+    });
 
-        MashupPlatform.wiring.registerCallback('ooi-types_in', function (data) {
-            setObjectsOfInterestTypes(JSON.parse(data));
-        });
+    MashupPlatform.wiring.registerCallback('ooi-types_in', function (data) {
+        setObjectsOfInterestTypes(JSON.parse(data));
+    });
 
-        MashupPlatform.wiring.registerCallback('point_in', function (data) {
-            executePendingWith(JSON.parse(data), { failSilently: true });
-        });
+    MashupPlatform.wiring.registerCallback('point_in', function (data) {
+        executePendingWith(JSON.parse(data), { failSilently: true });
+    });
 
+    $(function () {
         $('body')
             .on('command', function (event, data) {
                 MashupPlatform.wiring.pushEvent('commands', JSON.stringify(data));
@@ -31,5 +35,5 @@ $(function () {
             .on('areaCreated', function (event, data) {
                 MashupPlatform.wiring.pushEvent('areas_created_out', JSON.stringify(data));
             });
-    }
-});
+    });
+}
