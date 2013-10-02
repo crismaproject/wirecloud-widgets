d45e66a2e25954a433f89c5caacc831351083319
Manuel Warum <manuel.warum.fl@ait.ac.at>
2013-10-02 13:24:12 +0200

    ooi_command: slight improvement in code readability

--- ooi_command/js/main.js
+++ ooi_command/js/main.js
@@ -18,7 +18,7 @@ function setObjectsOfInterest(oois) {
  * @param {Array} types
  */
 function setObjectsOfInterestTypes(types) {
-    entityTypes = types.group('entityTypeId');
+    entityTypes = types.toDict('entityTypeId');
 }
 
 /**
@@ -108,8 +108,8 @@ function rebuildUI() {
         container.append(createFieldsetFor('*'));
 
     for (var ooiType in objectsOfInterest) {
-        var displayName = entityTypes[ooiType] && entityTypes[ooiType][0] && entityTypes[ooiType][0]['entityTypeName'] ?
-            entityTypes[ooiType][0]['entityTypeName'] : '';
+        var displayName = entityTypes[ooiType] && entityTypes[ooiType]['entityTypeName'] ?
+            entityTypes[ooiType]['entityTypeName'] : '';
         container.append(createFieldsetFor(ooiType, displayName));
     }
 }
@@ -203,6 +203,28 @@ Array.prototype.group = function(keyProperty) {
             groups[key] = [ obj ];
         else
             groups[key].push(obj);
+    }
+
+    return groups;
+};
+
+/**
+ * Groups the provided array of objects into an object where the object's properties are values extracted
+ * from the keyProperty property of array elements, and each keyed entry in the object is the first
+ * element of the original array sharing the same key.
+ *
+ * @param {string} keyProperty
+ * @returns {{}}
+ */
+Array.prototype.toDict = function(keyProperty) {
+    var groups = { };
+
+    for (var i = 0; i < this.length; i++) {
+        var obj = this[i];
+        if (!obj.hasOwnProperty(keyProperty)) continue;
+        var key = obj[keyProperty];
+        if (!(key in groups))
+            groups[key] = obj;
     }
 
     return groups;
