38acc497528fe7385b6876e19b4de0f87d62717c
Manuel Warum <manuel.warum.fl@ait.ac.at>
2013-10-04 12:55:18 +0200

    chart: updating to current endpoint naming and property scheme

--- chart/js/wirecloud.js
+++ chart/js/wirecloud.js
@@ -2,21 +2,22 @@
 /*
  * Iff the Wirecloud environment is available, register endpoints.
  */
-$(function () {
-    if (typeof MashupPlatform === 'undefined') {
-        console.warn('Wirecloud environment not detected.');
-    } else if (typeof series === 'undefined') {
-        console.warn('"series" variable is not defined.');
-    } else {
-        var applyPreferences = function () {
-            window.indicator_uri = MashupPlatform.prefs.get('indicator-uri');
-        };
+if (typeof MashupPlatform === 'undefined') {
+    console.warn('Wirecloud environment not detected.');
+} else if (typeof series === 'undefined') {
+    console.warn('"series" variable is not defined.');
+} else {
+    var applyPreferences = function () {
+        window.indicator_uri = MashupPlatform.prefs.get('indicator-uri');
+    };
 
-        MashupPlatform.prefs.registerCallback(applyPreferences);
-        applyPreferences();
+    MashupPlatform.prefs.registerCallback(applyPreferences);
+    applyPreferences();
 
-        MashupPlatform.wiring.registerCallback('worldstate', function (data) {
-            loadWorldState(series, data);
-        });
-    }
-});
+    MashupPlatform.wiring.registerCallback('worldstate_in', function (data) {
+        var worldstate = JSON.parse(data);
+        if (!worldstate || !worldstate.hasOwnProperty('worldStateId'))
+            throw 'Provided worldstate object has no worldStateId property.';
+        loadWorldState(series, worldstate.worldStateId);
+    });
+}
