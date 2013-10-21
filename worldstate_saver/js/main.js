20432d52c3a9355fc754aad4e5d54db5846af903
Manuel Warum <manuel.warum.fl@ait.ac.at>
2013-10-21 13:50:22 +0200

    worldstate_saver: new operator to write worldstates

---
+++ worldstate_saver/js/main.js
@@ -0,0 +1,61 @@
+/*global MashupPlatform*/
+
+var activeWorldState = null;
+var knownOOIs = { };
+var commandQueue = [ ];
+
+var apiUri = null;
+var applyPreferences = function () {
+    apiUri = MashupPlatform.prefs.get('api');
+};
+
+MashupPlatform.prefs.registerCallback(applyPreferences);
+applyPreferences();
+
+MashupPlatform.wiring.registerCallback('worldstate', function (data) {
+    activeWorldState = JSON.parse(data);
+});
+
+MashupPlatform.wiring.registerCallback('oois', function (data) {
+    knownOOIs = JSON.parse(data).toDict('entityId');
+});
+
+function createWorldState() {
+    var worldStateObj = {
+        worldStateId: -1,
+        simulationId: activeWorldState.simulationId,
+        worldStateParentId: activeWorldState.worldStateId,
+        dateTime: activeWorldState.dateTime
+    };
+    console.log(worldStateObj); // TODO: Send to OOI-WSR
+}
+
+function createOOIs() {
+    for (var i = 0; i < commandQueue.length; i++) {
+        var command = commandQueue[i];
+        // TODO: Process command
+        // TODO: Newly generated OOIs should be processed first and put into knownOOIs as soon as they have an ID
+    }
+}
+
+/**
+ * Groups the provided array of objects into an object where the object's properties are values extracted
+ * from the keyProperty property of array elements, and each keyed entry in the object is the first
+ * element of the original array sharing the same key.
+ *
+ * @param {string} keyProperty
+ * @returns {{}}
+ */
+Array.prototype.toDict = function(keyProperty) {
+    var groups = { };
+
+    for (var i = 0; i < this.length; i++) {
+        var obj = this[i];
+        if (!obj.hasOwnProperty(keyProperty)) continue;
+        var key = obj[keyProperty];
+        if (!(key in groups))
+            groups[key] = obj;
+    }
+
+    return groups;
+};
