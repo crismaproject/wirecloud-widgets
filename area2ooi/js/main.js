60a9463109f0c1bbd3548c9072711d3963d1bfd4
Manuel Warum <manuel.warum.fl@ait.ac.at>
2013-11-05 13:35:30 +0100

    area2ooi: only wire things if Wirecloud is present
    merger: only wire things if Wirecloud is present; also minor code improvements

--- area2ooi/js/main.js
+++ area2ooi/js/main.js
@@ -1,18 +1,20 @@
 /*global MashupPlatform*/
 
-MashupPlatform.wiring.registerCallback('areas_in', function (data) {
-    var areas = JSON.parse(data);
-    if (typeof areas == 'object')
-        areas = [ areas ];
-    var oois = areas.map(area2ooi);
+if (typeof MashupPlatform !== 'undefined') {
+    MashupPlatform.wiring.registerCallback('areas_in', function (data) {
+        var areas = JSON.parse(data);
+        if (typeof areas == 'object')
+            areas = [ areas ];
+        var oois = areas.map(area2ooi);
 
-    MashupPlatform.wiring.pushEvent('oois_out', JSON.stringify(oois));
-});
+        MashupPlatform.wiring.pushEvent('oois_out', JSON.stringify(oois));
+    });
 
-function area2ooi(area) {
-    return {
-        'entityId': area.entityId,
-        'entityTypeId': area.entityTypeId || 14,
-        'entityName': area.entityName || 'Area'
+    function area2ooi(area) {
+        return {
+            'entityId': area.entityId,
+            'entityTypeId': area.entityTypeId || 14,
+            'entityName': area.entityName || 'Area'
+        }
     }
 }
--- merger/js/main.js
+++ merger/js/main.js
@@ -1,25 +1,28 @@
 /*global MashupPlatform*/
 
-var a = [ ];
-var b = [ ];
+if (typeof MashupPlatform !== 'undefined') {
+    var a = [ ];
+    var b = [ ];
 
-function push() {
-    var merged;
-    if (!b.length && a.length)
-        merged = a;
-    else if (b.length && !a.length)
-        merged = b;
-    else
-        merged = a.concat(b);
-    MashupPlatform.wiring.pushEvent('output', JSON.stringify(merged));
+    function push() {
+        MashupPlatform.wiring.pushEvent('output', JSON.stringify(a.concat(b)));
+    }
+
+    MashupPlatform.wiring.registerCallback('a_in', function (data) {
+        try {
+            a = JSON.parse(data);
+            push();
+        } catch(e) {
+            console.log(e);
+        }
+    });
+
+    MashupPlatform.wiring.registerCallback('b_in', function (data) {
+        try {
+            b = JSON.parse(data);
+            push();
+        } catch(e) {
+            console.log(e);
+        }
+    });
 }
-
-MashupPlatform.wiring.registerCallback('a', function (data) {
-    a = JSON.parse(data);
-    push();
-});
-
-MashupPlatform.wiring.registerCallback('b', function (data) {
-    b = JSON.parse(data);
-    push();
-});
