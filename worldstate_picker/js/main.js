45831ab65ecf0d2639292f96b763162ddd9f0c0d
Manuel Warum <manuel.warum.fl@ait.ac.at>
2013-11-14 12:19:24 +0100

    worldstate_picker: experimental impl using the JavaScript InfoVis Toolkit

--- worldstate_picker/js/main.js
+++ worldstate_picker/js/main.js
@@ -1,103 +1,170 @@
-var apiUri = null;
+var api = null;
+/** @const */
+var treeStyle = {
+    colors: {
+        path: '#E6677A',
+        leaf: '#E9E9E9',
+        nonLeaf: '#F2919D',
+        edge: '#aaaaaa'
+    },
+    sizes: {
+        nodeWidth: 108,
+        levelDistance: 54
+    }
+};
 
-function createWorldStateTree(containerName, wsid, renderOptions) {
-    // create tree from WS list
-    $.getJSON (apiUri + '/WorldState', function (list) {
-        var treeData = getFromList (wsid, list);
+function createWorldStateTree(containerName, simulation) {
+    if (!api) throw 'RESTful service not configured yet.';
 
-        // build the options object
-        var options = $.extend({
-            nodeRadius: 5, fontSize: 12
-        }, renderOptions);
+    api.listWorldStates()
+        .done(function (worldStates) {
+            var jitData = toJit(worldStates, simulation);
+            var st = new $jit.ST({
+                //id of viz container element
+                injectInto: containerName,
+                orientation: 'top',
+                levelsToShow: 5,
+                //set animation transition type
+                transition: $jit.Trans.Quart.easeInOut,
+                //set distance between node and its children
+                levelDistance: treeStyle.sizes.levelDistance,
+                //enable panning
+                Navigation: {
+                    enable:true,
+                    panning:true
+                },
+                //set node and edge styles
+                //set overridable=true for styling individual
+                //nodes or edges
+                Node: {
+                    height: 20,
+                    width: treeStyle.sizes.nodeWidth,
+                    type: 'rectangle',
+                    color: treeStyle.colors.leaf,
+                    overridable: true
+                },
 
-        // Calculate total nodes, max label length
-        var totalNodes = 0;
-        var maxLabelLength = 0;
-        visit(treeData,
-            function(d) {
-                totalNodes++;
-                maxLabelLength = Math.max(d.label.length, maxLabelLength);
-            },
-            function(d) { return d.children && d.children.length > 0 ? d.children : null; });
+                Edge: {
+                    type: 'bezier',
+                    color: treeStyle.colors.edge,
+                    overridable: true
+                },
 
-        // size of the diagram
-        var size = { width:$(containerName).outerWidth(), height: totalNodes * 15};
+                //This method is called right before plotting
+                //a node. It's useful for changing an individual node
+                //style properties before plotting it.
+                //The data properties prefixed with a dollar
+                //sign will override the global node style properties.
+                onBeforePlotNode: function(node){
+                    //add some color to the nodes in the path between the
+                    //root node and the selected node.
+                    if (node.selected) {
+                        node.data.$color = treeStyle.colors.path;
+                    }
+                    else {
+                        delete node.data.$color;
+                        //if the node belongs to the last plotted level
+                        if(!node.anySubnode("exist")) {
+                            //count children number
+                            var count = 0;
+                            node.eachSubnode(function () { count++; });
+                            if (count) node.data.$color = treeStyle.colors.nonLeaf;
+                        }
+                    }
+                },
 
-        var tree = d3.layout.tree()
-            .sort(null)
-            .size([size.height, size.width - maxLabelLength*options.fontSize])
-            .children(function(d) { return (!d.children || d.children.length === 0) ? null : d.children; });
+                //This method is called on DOM label creation.
+                //Use this method to add event handlers and styles to
+                //your node.
+                onCreateLabel: function(label, node){
+                    label.id = node.id;
+                    label.innerHTML = node.name;
+                    label.onclick = function(){ st.onClick(node.id); };
 
-        var nodes = tree.nodes(treeData);
-        var links = tree.links(nodes);
+                    var style = label.style;
+                    style.width = treeStyle.sizes.nodeWidth + 'px';
+                    style.height = 17 + 'px';
+                    style.color = '#000000';
+                    style.textAlign = 'center';
+                }
+            });
+            //load json data
+            st.loadJSON(jitData);
+            //compute node positions and layout
+            st.compute();
+            //emulate a click on the root node.
+            st.onClick(st.root);
+        });
+}
 
-        /*
-         <svg>
-         <g class="container" />
-         </svg>
-         */
-        var layoutRoot = d3.select(containerName)
-            .append("svg:svg").attr("width", size.width).attr("height", size.height)
-            .append("svg:g")
-            .attr("class", "container")
-            .attr("transform", "translate(" + maxLabelLength + ",0)");
+function toJit(worldStates, simulation) {
+    var simulationId = simulation.simulationId;
+    worldStates = worldStates
+        .filter(function (x) { return x.simulationId == simulationId; })
+        .map(function (x) { return $.extend({children: []}, x); });
+    var worldStatesLookup = linkWorldStates(worldStates);
 
+    return {
+        id: 's' + simulationId,
+        name: 'Simulation ' + simulationId,
+        //data: simulation,
+        children: worldStates
+            .filter(function (x) { return x.worldStateParentId === null; })
+            .map(function (x) { return toJitNode(worldStatesLookup, x.worldStateId) })
+    };
+}
 
-        // Edges between nodes as a <path class="link" />
-        var link = d3.svg.diagonal()
-            .projection(function(d) { return [d.y, d.x]; });
+function toJitNode(worldStatesLookup, nodeId) {
+    var node = worldStatesLookup[nodeId];
+    return {
+        id: 'w' + nodeId,
+        name: 'WorldState ' + nodeId,
+        //data: node,
+        children: node.children.map(function (childId) { return toJitNode(worldStatesLookup, childId); })
+    };
+}
 
-        layoutRoot.selectAll("path.link")
-            .data(links)
-            .enter()
-            .append("svg:path")
-            .attr("class", "link")
-            .attr("d", link);
-
-
-        /*
-         Nodes as
-         <g class="node">
-         <circle class="node-dot" />
-         <text />
-         </g>
-         */
-        var nodeGroup = layoutRoot.selectAll("g.node")
-            .data(nodes)
-            .enter()
-            .append("svg:g")
-            .attr("class", "node")
-            .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; });
-
-        nodeGroup.append("svg:circle")
-            .attr("class", "node-dot")
-            .attr("r", options.nodeRadius)
-            .on ("click", function (d) {alert (d.wsid)});
-
-        nodeGroup.append("svg:text")
-            .attr("text-anchor", function(d) { return d.children ? "end" : "start"; })
-            .attr("dx", function(d)
-            {
-                var gap = 2 * options.nodeRadius;
-                return d.children ? -gap : gap;
-            })
-            .attr("dy", 3)
-            .text(function(d) { return d.label; });
+function linkWorldStates(worldStates) {
+    var worldStatesDict = worldStates.toDict('worldStateId', function (worldState) {
+        return $.extend({children: []}, worldState);
     });
+    for (var i = 0; i < worldStates.length; i++) {
+        var worldState = worldStates[i];
+        if (!worldState.worldStateParentId) continue;
+        var parent = worldStatesDict[worldState.worldStateParentId];
+        parent.children.push(worldState.worldStateId);
+    }
+    return worldStatesDict;
 }
 
-function getFromList (wsid, list) {
-    var tmp = { wsid : wsid, children : [], label: 'X' };
-    for (var i = 0; i < list.length; i++)
-        if (list[i]['worldStateParentId'] === wsid)
-            tmp['children'].push(getFromList(list[i]['worldStateId'], list));
-    return tmp;
-}
+console.logTree = function (jitTreeNode) {
+    function log(node, prefix) {
+        var str = prefix + node.id + '\n';
+        for (var i = 0; i < node.children.length; i++)
+            str += log(node.children[i], prefix + node.id +  '/');
+        return str;
+    }
+    console.log(log(jitTreeNode, ''));
+};
 
-function visit(parent, visitFn, childrenFn) {
-    if (!parent) return;
-    visitFn(parent);
-    var children = childrenFn(parent);
-    if (children)
-        children.forEach(function(child) { visit(child, visitFn, childrenFn); });
-}
+/**
+ * Groups the provided array of objects into an object where the object's properties are values extracted
+ * from the keyProperty property of array elements, and each keyed entry in the object is the first
+ * element of the original array sharing the same key.
+ *
+ * @param {string} keyProperty
+ * @returns {{}}
+ */
+Array.prototype.toDict = function(keyProperty) {
+    var groups = { };
+
+    for (var i = 0; i < this.length; i++) {
+        var obj = this[i];
+        if (!obj.hasOwnProperty(keyProperty)) continue;
+        var key = obj[keyProperty];
+        if (!(key in groups))
+            groups[key] = obj;
+    }
+
+    return groups;
+};
--- worldstate_picker/js/wirecloud.js
+++ worldstate_picker/js/wirecloud.js
@@ -7,15 +7,14 @@ if (typeof MashupPlatform === 'undefined') {
     console.warn('Wirecloud environment not detected.');
 } else {
     var applyPreferences = function () {
-        apiUri = MashupPlatform.prefs.get('api');
+        var api = new WorldStateRepository(MashupPlatform.prefs.get('api'));
     };
 
     MashupPlatform.prefs.registerCallback(applyPreferences);
     applyPreferences();
 
-    var jqueryGet = $.fn.get;
-    $.fn.get = function (uri, callback, type) {
-        var proxyUri = MashupPlatform.http.buildProxyURL(uri);
-        return jqueryGet(proxyUri, callback, type);
-    };
+    MashupPlatform.wiring.registerCallback('simulation', function (simulation) {
+        var simulation = JSON.parse(simulation);
+
+    });
 }
