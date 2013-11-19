c7b0cda071c468c99caa107076677823deefc491
Manuel Warum <manuel.warum.fl@ait.ac.at>
2013-11-19 13:22:47 +0100

    listener: styling
    listener: showing when rows where added
    
    listener: can now be limited to N rows (default N=100)

--- debug_listener/js/main.js
+++ debug_listener/js/main.js
@@ -1,7 +1,62 @@
+var rowLimit = 100;
+
 /**
  * Adds a line to the top of the event log.
  * @param {object} data the contents of the line to add; it will always be converted to a string.
+ * @param {string?} cssClass
  */
-function appendData(data) {
-    $('#data').prepend($('<li></li>').text(data.toString()));
+function appendData(data, cssClass) {
+    var row = $('<li></li>')
+            .append($('<time></time>').text(currentTime()))
+            .append($('<span></span>')
+                .addClass('raw')
+                .text(data.toString())
+            )
+        .hide();
+
+    if (cssClass) row.addClass(cssClass);
+
+    var $data = $('#data');
+    $data.prepend(row);
+
+    row.fadeIn();
+
+    $('li:gt(' + (rowLimit-1) + ')', $data).remove();
 }
+
+/**
+ * Returns the current local time as a string following the HH:MM:SS.mmmm format
+ * @returns {string}
+ */
+function currentTime() {
+    function pad(s, len) {
+        if (typeof s !== 'string')
+            s = s.toString();
+        return s.length < len ? '0'.repeat(len - s.length) + s : s;
+    }
+    var d = new Date();
+    return '{0}:{1}:{2}.{3}'.format(pad(d.getHours(), 2), pad(d.getMinutes(), 2), pad(d.getSeconds(), 2), pad(d.getMilliseconds(), 4));
+}
+
+if (!String.prototype.repeat)
+    /**
+     * @param {number} num
+     * @returns {string}
+     */
+    String.prototype.repeat = function(num) {
+        return new Array(num + 1).join(this);
+    };
+
+if (!String.prototype.format)
+    /**
+     * @returns {string}
+     */
+    String.prototype.format = function () {
+        var args = arguments;
+        return this.replace(/{(\d+)}/g, function (match, number) {
+            return typeof args[number] !== 'undefined'
+                ? args[number]
+                : match
+                ;
+        });
+    };
--- debug_listener/js/wirecloud.js
+++ debug_listener/js/wirecloud.js
@@ -3,22 +3,25 @@
 /*
  * Iff the Wirecloud environment is available, register endpoints.
  */
-if (typeof MashupPlatform === 'undefined')
+if (typeof MashupPlatform === 'undefined') {
     console.warn('Wirecloud environment not detected.');
+    $(function() { appendData('The Wirecloud platform was not detected!', 'text-danger'); });
+} else {
+    var toConsole = false;
+    var applyPreferences = function () {
+        toConsole = MashupPlatform.prefs.get('to_console');
+        rowLimit = MashupPlatform.prefs.get('row_limit');
+    };
 
-var toConsole = false;
-var applyPreferences = function () {
-    toConsole = MashupPlatform.prefs.get('to_console');
-};
+    MashupPlatform.prefs.registerCallback(applyPreferences);
+    applyPreferences();
 
-MashupPlatform.prefs.registerCallback(applyPreferences);
-applyPreferences();
-
-MashupPlatform.wiring.registerCallback('data', function (data) {
-    appendData(data);
-    if (toConsole)
-        console.log({
-            'received': true,
-            'data': data
-        });
-});
+    MashupPlatform.wiring.registerCallback('data', function (data) {
+        appendData(data);
+        if (toConsole)
+            console.log({
+                'received': true,
+                'data': data
+            });
+    });
+}
