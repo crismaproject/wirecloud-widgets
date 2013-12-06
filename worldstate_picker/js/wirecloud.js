4d9cc8560ccb3d65a19d26c6359247f3f2b1f960
Manuel Warum <manuel.warum.fl@ait.ac.at>
2013-12-06 13:16:01 +0100

    worldstate_picker: now with a "load worldstate" button

--- worldstate_picker/js/wirecloud.js
+++ worldstate_picker/js/wirecloud.js
@@ -14,7 +14,39 @@ if (typeof MashupPlatform === 'undefined') {
     applyPreferences();
 
     MashupPlatform.wiring.registerCallback('simulation', function (simulation) {
-        var simulation = JSON.parse(simulation);
+        simulation = JSON.parse(simulation);
         createWorldStateTree('worldstate-tree', simulation);
     });
 }
+
+$(function () {
+    var lastSelectedNode = null;
+
+    $('#worldstate-tree').on('nodeSelected', function (a, b) {
+        lastSelectedNode = b;
+    });
+
+    $('#loadBtn').click(function () {
+        if (!lastSelectedNode) return;
+
+        MashupPlatform.wiring.pushEvent('worldstate', JSON.stringify(lastSelectedNode));
+        api.listEntities()
+            .done(function (response) {
+                // BEGIN workaround: not all entities belong to the WorldState; ignoring all that don't have properties in the given worldstate
+                var oois = response.filter(function(x) {
+                    return x.hasOwnProperty('entityInstancesProperties') &&
+                        x.entityInstancesProperties.length &&
+                        x.entityInstancesProperties.every(function (y) {
+                            return y.worldStateId == lastSelectedNode.worldStateId;
+                        });
+                });
+                // END workaround
+                MashupPlatform.wiring.pushEvent('oois', JSON.stringify(oois));
+            });
+
+        api.listEntityTypes()
+            .done(function (response) {
+                MashupPlatform.wiring.pushEvent('ooi-types', JSON.stringify(response));
+            });
+    });
+});
