d44ade88e0802c5b1ea7ba7fb2edf2dd1549f284
Manuel Warum <manuel.warum.fl@ait.ac.at>
2014-02-26 13:55:11 +0100

    Adding empty widget template for new widgets

---
+++ empty/js/wirecloud.js
@@ -0,0 +1,6 @@
+if (typeof MashupPlatform === 'undefined') {
+    // we are not within Wirecloud, the API wasn't injected! Someone probably opened your HTML file directly.
+    console.warn('The Wirecloud environment was not detected.');
+} else {
+    // wirings, preferences, etc.
+}
