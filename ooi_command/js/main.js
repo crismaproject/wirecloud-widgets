8b60189b78bcfb2ad298aa0313f10887f11321ca
Manuel Warum <manuel.warum.fl@ait.ac.at>
2014-03-12 16:50:02 +0100

    ooi_command: separating JS into more files for clarity

--- ooi_command/js/main.js
+++ ooi_command/js/main.js
@@ -3,7 +3,7 @@ var entityTypes = { }, objectsOfInterest = { }, pendingCommand = null;
 /**
  * @private
  * @const
- * */
+ **/
 var uiStyle = {
     btnClass: 'btn btn-default btn-sm'
 };
@@ -202,7 +202,7 @@ function executeCommand(command, data) {
 }
 
 function spawnArea(areaPrototype, geometry) {
-    return $.extend(true, {}, {
+    return $.extend(true, {
         'entityId': -(sequence++),
         'entityTypeId': 14,
         'entityInstancesGeometry': [
@@ -247,67 +247,6 @@ function executePendingWith(data, options) {
     executeCommand(pendingCommand, commandData);
     cancelPendingCommand();
 }
-
-/**
- * Groups the provided array of objects into an object where the object's properties are values extracted
- * from the keyProperty property of array elements, and each keyed entry in the object is an array of
- * elements sharing the same key.
- *
- * @param {string} keyProperty
- * @returns {{}}
- */
-Array.prototype.group = function(keyProperty) {
-    var groups = { };
-
-    for (var i = 0; i < this.length; i++) {
-        var obj = this[i];
-        if (!obj.hasOwnProperty(keyProperty)) continue;
-        var key = obj[keyProperty];
-        if (!(key in groups))
-            groups[key] = [ obj ];
-        else
-            groups[key].push(obj);
-    }
-
-    return groups;
-};
-
-/**
- * Groups the provided array of objects into an object where the object's properties are values extracted
- * from the keyProperty property of array elements, and each keyed entry in the object is the first
- * element of the original array sharing the same key.
- *
- * @param {string} keyProperty
- * @returns {{}}
- */
-Array.prototype.toDict = function(keyProperty) {
-    var groups = { };
-
-    for (var i = 0; i < this.length; i++) {
-        var obj = this[i];
-        if (!obj.hasOwnProperty(keyProperty)) continue;
-        var key = obj[keyProperty];
-        if (!(key in groups))
-            groups[key] = obj;
-    }
-
-    return groups;
-};
-
-/**
- * Returns only elements of the specified array that satisfy the specified predicate.
- *
- * @param {function} predicate a function that tests an element of the array and returns true iff it is to be included
- * in the returned array; otherwise it will not be included.
- * @returns {Array} an array where every element of the original array is included iff the predicate returned true.
- */
-Array.prototype.only = function(predicate) {
-    var newArray = [ ];
-    for (var i = 0; i < this.length; i++)
-        if (predicate(this[i]))
-            newArray.push(this[i]);
-    return newArray;
-};
 
 $(function () {
     $(document).keyup(function (eventData) {
