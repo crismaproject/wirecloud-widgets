0ca1ff4eb04b4c890fcbed3e57bead18afddcda9
Manuel Warum <manuel.warum.fl@ait.ac.at>
2014-03-24 10:47:42 +0100

    Merging worldstate picker with simulation picker and remodeling the widget as a angular application

--- worldstate_picker/js/main.js
+++ worldstate_picker/js/main.js
@@ -1,250 +1,118 @@
-var api = null;
-var layout = { };
-/** @const */
-treeCfg = {
-    colors: {
-        path: '#E6677A',
-        leaf: '#E9E9E9',
-        intermediateNode: '#F2919D',
-        edge: '#aaaaaa'
-    },
-    orientation: 'left',
-    sizes: {
-        nodeWidth: 60,
-        levelDistance: 14
-    },
-    data: [ 'simulationId', 'worldStateId', 'dateTime', 'description' ]
-};
-
-var setLayout = function ($container, st) {
-    layout.w = $container.width();
-    layout.h = $container.height();
-    layout.tree = st;
-    layout.container = $container;
-};
-/**
- * Creates the visual tree representation of worldstates associated with the specified simulation.
- * @param {string} containerName the DOM identifier of the container that will host the tree.
- * @param {object} simulation the simulation object.
- */
-function createWorldStateTree(containerName, simulation) {
-    if (!api) throw 'RESTful service not configured yet.';
-    var $notification = $('#loadingNotification');
-    $notification.modal('show');
-
-    api.listWorldStates()
-        .done(function (worldStates) {
-            var $container = $('#' + containerName);
-            $container.empty();
-            worldStates = worldStates.filter(function (x) { return x.simulationId == simulation.simulationId; });
-            window.worldStateCache = worldStates.toDict('worldStateId');
-
-            var jitData = toJit(worldStates, simulation);
-            var st = new $jit.ST({
-                //id of viz container element
-                injectInto: containerName,
-                orientation: treeCfg.orientation,
-                levelsToShow: 10,
-                //set animation transition type
-                transition: $jit.Trans.Quart.easeInOut,
-                //set distance between node and its children
-                levelDistance: treeCfg.sizes.levelDistance,
-                siblingOffset: 1,
-                subtreeOffset: 2,
-                //enable panning
-                Navigation: {
-                    enable: true,
-                    panning: true
-                },
-                //set node and edge styles
-                Node: {
-                    height: 20,
-                    width: treeCfg.sizes.nodeWidth,
-                    type: 'rectangle',
-                    color: treeCfg.colors.leaf,
-                    overridable: true
-                },
-
-                Edge: {
-                    type: 'bezier',
-                    color: treeCfg.colors.edge,
-                    overridable: true
-                },
-
-                //This method is called right before plotting
-                //a node. It's useful for changing an individual node
-                //style properties before plotting it.
-                //The data properties prefixed with a dollar
-                //sign will override the global node style properties.
-                onBeforePlotNode: function (node) {
-                    //add some color to the nodes in the path between the
-                    //root node and the selected node.
-                    if (node.selected) {
-                        node.data.$color = treeCfg.colors.path;
-                    }
-                    else {
-                        delete node.data.$color;
-                        //if the node belongs to the last plotted level
-                        if (!node.anySubnode("exist")) {
-                            //count children number
-                            var count = 0;
-                            node.eachSubnode(function () {
-                                count++;
-                            });
-                            if (count) node.data.$color = treeCfg.colors.intermediateNode;
-                        }
-                    }
-                },
-
-                //This method is called on DOM label creation.
-                //Use this method to add event handlers and styles to
-                //your node.
-                onCreateLabel: function (label, node) {
-                    label.id = node.id;
-                    label.innerHTML = node.name;
-                    label.onclick = function(){
-                        st.onClick(node.id);
-
-                        var propagateData = { };
-                        for (var i = 0; i < treeCfg.data.length; i++) {
-                            var key = treeCfg.data[i];
-                            if (node.data.hasOwnProperty(key))
-                                propagateData[key] = node.data[key];
-                        }
-
-                        $container.trigger('nodeSelected', propagateData);
-                    };
-
-                    var style = label.style;
-                    style.width = treeCfg.sizes.nodeWidth + 'px';
-                    style.height = 17 + 'px';
-                    style.color = '#000000';
-                    style.textAlign = 'center';
-                }
-            });
-            //load json data
-            st.loadJSON(jitData);
-            //compute node positions and layout
-            st.compute();
-            //emulate a click on the root node.
-            st.onClick(st.root);
-
-            setLayout($container, st);
-        })
-        .always(function () {
-            $notification.modal('hide');
-        });
-}
-
-/**
- * @param {Array} worldStates an array of (possibly unassociated) World States.
- * @param {Object} simulation the simulation for which to generate the tree.
- * @returns {{id: string, name: string, data: *, children: Array}}
- */
-function toJit(worldStates, simulation) {
-    var simulationId = simulation.simulationId;
-    worldStates = worldStates.map(function (x) { return $.extend({children: []}, x); });
-    var worldStatesLookup = linkWorldStates(worldStates);
-
-    return {
-        id: 's' + simulationId,
-        name: 'Sim. ' + simulationId,
-        data: simulation,
-        children: worldStates
-            .filter(function (x) { return x.worldStateParentId === null; })
-            .map(function (x) { return toJitNode(worldStatesLookup, x.worldStateId) })
-    };
-}
-/**
- * Creates a JIT-compatible JSON object for the specified WorldState.
- * Note that this function is recursive. It will also create these objects for any declared children.
- * @param {Object} worldStatesLookup the WorldState dictionary as created by linkWorldStates
- * @param {number} nodeId the unique identifier of the WorldState for which to generate the JSON object
- * @returns {{id: string, name: string, data: *, children: Array}}
- */
-function toJitNode(worldStatesLookup, nodeId) {
-    var node = worldStatesLookup[nodeId];
-    return {
-        id: 'w' + nodeId,
-        name: 'Ws. ' + nodeId,
-        data: node,
-        children: node.children.map(function (childId) { return toJitNode(worldStatesLookup, childId); })
-    };
-}
-
-/**
- * This method creates a dictionary-version of an WorldState array where each key corresponds to the
- * WorldState's own identifier. In addition, it creates and populates a 'children' property for each WorldState
- * that contains identifiers to any other WorldState that declares it as its parent.
- *
- * Note that this method will raise silent warnings in your JavaScript console if any inconsistencies were found
- * while processing the specified array of world states.
- *
- * @param {Array} worldStates
- * @returns {Object}
- */
-function linkWorldStates(worldStates) {
-    var worldStatesDict = worldStates.toDict('worldStateId', function (worldState) {
-        return $.extend({children: []}, worldState);
-    });
-    for (var i = 0; i < worldStates.length; i++) {
-        var worldState = worldStates[i];
-        if (!worldState.worldStateParentId) continue;
-        var parent = worldStatesDict[worldState.worldStateParentId];
-        if (!parent)
-            console.warn('World state ' + worldState.worldStateParentId + ' was not found, but it is apparently a parent of world state ' + worldState.worldStateId);
-        else
-            parent.children.push(worldState.worldStateId);
-    }
-    return worldStatesDict;
-}
-
-function getHistoryOf(worldStateId) {
-    var history = [ ];
-    for (var current = window.worldStateCache[worldStateId]; current != null; current = window.worldStateCache[current.worldStateParentId])
-        history = [ current ].concat(history);
-    return history;
-}
-
-/**
- * Groups the provided array of objects into an object where the object's properties are values extracted
- * from the keyProperty property of array elements, and each keyed entry in the object is the first
- * element of the original array sharing the same key.
- *
- * @param {string} keyProperty
- * @returns {{}}
- */
-Array.prototype.toDict = function (keyProperty) {
-    var groups = { };
-
-    for (var i = 0; i < this.length; i++) {
-        var obj = this[i];
-        if (!obj.hasOwnProperty(keyProperty)) continue;
-        var key = obj[keyProperty];
-        if (!(key in groups))
-            groups[key] = obj;
-    }
-
-    return groups;
-};
-
-$(function () {
-    var $worldstate = $('#worldstate-info');
-    $worldstate.hide();
-
-    $('#worldstate-tree').on('nodeSelected', function (event, selection) {
-        if (!selection.hasOwnProperty('worldStateId')) return;
-        $worldstate.show();
-        $worldstate.find('.title').text('World State ' + selection.worldStateId);
-        $worldstate.find('.dateTime').text(selection.dateTime);
-    });
-
-    $(window).resize(function () {
-        if (layout && layout.tree && (layout.container.width() != layout.w || layout.container.height() != layout.h)) {
-            var dx = layout.container.width() - layout.w;
-            var dy = layout.container.height() - layout.h;
-            layout.tree.canvas.translate(dx/2, dy/2);
-            setLayout(layout.container, layout.tree);
+angular.module('worldStatePickerApp', [])
+    .factory('ooiwsr', function() {
+        var ooiwsr = new WorldStateRepository('');
+        if (typeof MashupPlatform !== 'undefined') {
+            var applyPreferences = function () { ooiwsr.apiUri = MashupPlatform.prefs.get('api'); };
+            MashupPlatform.prefs.registerCallback(applyPreferences);
+            applyPreferences();
         }
-    });
-});
+        return ooiwsr;
+    })
+    .controller('WorldStatePickerApp', ['$scope', 'ooiwsr', function($scope, ooiwsr) {
+        $scope.busy = false;
+        $scope.loaded = null;
+
+        $scope.simulationList = [];
+        $scope.selectedSimulation = null;
+
+        $scope.worldStateList = [];
+        $scope.selectedWorldState = null;
+
+        $scope.prettySimulation = function(simulation) {
+            return simulation.description + ' (' + simulation.simulationId + ')';
+        };
+        $scope.prettyWorldState = function(worldState) {
+            var meta = [];
+            if (worldState.dateTime) meta.push(new Date(worldState.dateTime).toLocaleString());
+            if (worldState.description) meta.push(worldState.description);
+
+            return meta.length ?
+                worldState.worldStateId + ' (' + meta.join('; ') + ')' :
+                worldState.worldStateId;
+        };
+
+        $scope.refreshWorldStates = function() {
+            $scope.busy = true;
+
+            ooiwsr.listWorldStates()
+                .done(function(ws){
+                    var i;
+                    var array = [];
+                    for(i = 0; i < ws.length; i++)
+                        array[ws[i].worldStateId] = $.extend({children: [], isLeaf: true}, ws[i]);
+                    for(i=0;i<array.length;i++){
+                        if(!array[i]) continue;
+                        var $me = array[i];
+                        if($me.worldStateParentId) {
+                            array[$me.worldStateParentId].children.push($me);
+                            array[$me.worldStateParentId].isLeaf = false;
+                        }
+                    }
+
+                    $scope.worldStateList = array;
+                })
+                .always(function() {
+                    $scope.busy = false;
+                    $scope.$apply();
+                });
+        };
+
+        $scope.refreshSimulations = function() {
+            $scope.busy = true;
+
+            ooiwsr.listSimulations()
+                .done(function(sims){
+                    $scope.simulationList = sims;
+                })
+                .always(function() {
+                    $scope.busy = false;
+                    $scope.$apply();
+                });
+        };
+
+        $scope.loadWorldState = function() {
+            var progressCurrent = 0, progressMax = 4;
+            var $progressBar = $('#progressBar');
+            var $progressBarContainer = $('#progressBarContainer');
+
+            var advanceProgress = function() {
+                progressCurrent++;
+                var options = { };
+                if (progressCurrent >= progressMax) {
+                    options.done = function() { $progressBarContainer.modal('hide'); };
+                    $progressBar.addClass('progress-bar-success');
+                }
+                var progress = { width: (progressCurrent / progressMax) * 100 + '%' };
+
+                $progressBar.animate(progress, options);
+            };
+
+            $progressBarContainer.modal('show');
+            $progressBar.removeClass('progress-bar-success');
+            send('simulation', $scope.selectedSimulation); advanceProgress();
+            send('worldState', $scope.selectedWorldState); advanceProgress();
+            $scope.loaded = $scope.selectedWorldState;
+            ooiwsr.listEntityTypes()
+                .done(function(ooiTypes) { send('ooi-types', ooiTypes); advanceProgress(); });
+            ooiwsr.fetch('/Entity?wsid=' + $scope.selectedWorldState.worldStateId)
+                .done(function(oois) { send('oois', oois); advanceProgress(); });
+        };
+
+        $scope.refreshSimulations();
+        $scope.refreshWorldStates();
+        $('#loadedContainer').modal('show');
+    }]);
+
+function send(wiringName, data) {
+    if (typeof MashupPlatform !== 'undefined') {
+        if (typeof data !== 'string')
+            data = JSON.stringify(data);
+        MashupPlatform.wiring.pushEvent(wiringName, data);
+    } else {
+        if (!this.hasOwnProperty('$warned')) {
+            this['$warned'] = true;
+            console.warn('Wirecloud is not available. Data sent to OutputEndpoints will be sent to the console instead.');
+        }
+        console.log([wiringName, data]);
+    }
+}
