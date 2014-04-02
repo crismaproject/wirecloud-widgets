crismaproject/wirecloud-widgets

commit 570cb81f02fb8c25a1e47d291a5e9a14df068739

a few code inspections across the board

widget-rdf.xslt
@@ -1,12 +1,10 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
                 xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-                xmlns:usdl-core="http://www.linked-usdl.org/ns/usdl-core#"
                 xmlns:foaf="http://xmlns.com/foaf/0.1/"
                 xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"
                 xmlns:dcterms="http://purl.org/dc/terms/"
                 xmlns:wire="http://wirecloud.conwet.fi.upm.es/ns/widget#"
-                xmlns:vCard="http://www.w3.org/2006/vcard/ns#"
                 xmlns:gr="http://purl.org/goodrelations/v1#">
   <xsl:output method="html" version="1.0" encoding="UTF-8" indent="yes"/>
   <xsl:template match="rdf:RDF">

indicators/index.html
@@ -42,7 +42,7 @@
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
                 <h4 class="modal-title"><span class="glyphicon glyphicon-tasks"></span> Select indicators</h4>
             </div>
             <div class="modal-body">

indicators/js/main.js
@@ -88,7 +88,7 @@ function createChart(containerWidth, containerHeight, data, colors, label) {
         .attr('class', 'hidden-print btn btn-danger btn-xs')
         .append($('<span class="glyphicon glyphicon-remove"></span>'));
     closeBtn.click(function() {
-        $self = $('.' + svgClassId);
+        var $self = $('.' + svgClassId);
         $self.slideUp({complete: function() {
             $self.remove();
             if (!$('.graph').length) $('#overlay').modal('show');

simulation_picker/css/main.css
@@ -7,23 +7,17 @@
 }
 
 #btn-load {
-    background-image: url('../img/load.png');
-    background-position: 6pt center;
-    background-repeat: no-repeat;
+    background: url('../img/load.png') no-repeat 6pt center;
     padding-left: 19pt;
 }
 
 #btn-refresh {
-    background-image: url('../img/refresh.png');
-    background-position: 6pt center;
-    background-repeat: no-repeat;
+    background: url('../img/refresh.png') no-repeat 6pt center;
     padding-left: 19pt;
 }
 
 .loadedIcon {
     display: inline-block;
     width: 17px; height: 17px;
-    background-image: url('../img/selectedIcon.png');
-    background-position: left bottom;
-    background-repeat: no-repeat;
+    background: url('../img/selectedIcon.png') no-repeat left bottom;
 }

worldstate_loader/config.xml
@@ -82,7 +82,7 @@
         <dcterms:description>Enforces the specified worldstate. The 'simulation id' input endpoint will be ignored. Use 0, if you want to disable this behaviour.</dcterms:description>
         <rdfs:label>Worldstate</rdfs:label>
         <wire:type>text</wire:type>
-        <wire:default></wire:default>
+        <wire:default/>
       </wire:PlatformPreference>
     </wire:hasPlatformPreference>
   </wire:Operator>

worldstate_picker/css/main.css
@@ -19,12 +19,8 @@ html, body { width: 100%; height: 100%; margin: 0; padding: 0; overflow: hidden 
     overflow-y: auto;
     padding-left: 1em;
     padding-right: 1em;
-    background-color: #f9f9f9;
-
     border-top: 1pt solid #dfdfdf;
-    background-image: url('../img/fade.png');
-    background-repeat: no-repeat;
-    background-position: top center;
+    background: #f9f9f9 url('../img/fade.png') no-repeat top center;
     z-index: 1;
 }
 

worldstate_picker/index.html
@@ -19,7 +19,7 @@
     <span class="dateTime"></span>
 </aside>
 
-<div class="modal fade" id="loadingNotification" tabindex="-1" role="dialog" aria-hidden="true">
+<div class="modal fade" id="loadingNotification" tabindex="-1" role="dialog">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">

commit e16cc5ec7cc313779e97fc40305d50f1b65648f6

570cb81 introduced an issue with RDF documentation generation. This is a partial revert of the original commit causing this.

widget-rdf.xslt
@@ -1,10 +1,12 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
                 xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+                xmlns:usdl-core="http://www.linked-usdl.org/ns/usdl-core#"
                 xmlns:foaf="http://xmlns.com/foaf/0.1/"
                 xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"
                 xmlns:dcterms="http://purl.org/dc/terms/"
                 xmlns:wire="http://wirecloud.conwet.fi.upm.es/ns/widget#"
+                xmlns:vCard="http://www.w3.org/2006/vcard/ns#"
                 xmlns:gr="http://purl.org/goodrelations/v1#">
   <xsl:output method="html" version="1.0" encoding="UTF-8" indent="yes"/>
   <xsl:template match="rdf:RDF">