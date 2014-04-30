ae1e3ed2e04ebfc7b8157399edcd06befa6c5557
Manuel Warum <manuel.warum.fl@ait.ac.at>
2014-04-30 10:09:31 +0200

    ooi_commands: adding pickup command

--- ooi_command/js/commands.js
+++ ooi_command/js/commands.js
@@ -25,14 +25,37 @@ angular.module('ooiCommand.commands', [])
             log: 'Create decontamination zone near lat. #{data.lat}, long. #{data.lon}'
         },
         {
+            id: 'pickup',
+            css: 'ico-cmd-goto',
+            entityTypeId: 7,
+            displayName: 'Pick up',
+            help: 'This command orders an ambulance to pick up patients around the specified location.',
+            targetType: 'ooi',
+            targetRestrictedTo: 14,
+            log: 'Retrieve patients from an area.',
+            isAvailable: function (ambulance) {
+                var properties = ambulance.entityInstancesProperties;
+                for (var i = 0; i < properties.length; i++) {
+                    var property = properties[i];
+                    // Check target availability time; 0 = now, >0 = later, <0 = never
+                    if (property.entityTypePropertyId == 312)
+                        return property.entityPropertyValue !== 0 && property.entityPropertyValue !== '0';
+                }
+                return true;
+            },
+            setProperties: {
+                314: '#{data.entityId}'
+            }
+        },
+        {
             id: 'dispatch',
             css: 'ico-cmd-goto',
             entityTypeId: 7,
-            displayName: 'Dispatch',
-            help: 'This command orders an ambulance to pick up patients and bring them to the specified hospital.',
+            displayName: 'Dispatch to hospital',
+            help: 'This command orders an ambulance to bring patients to the specified hospital.',
             targetType: 'ooi',
             targetRestrictedTo: 9,
-            log: 'Dispatch and haul patients to the hospital.',
+            log: 'Bring patients to the hospital.',
             isAvailable: function (ambulance) {
                 var properties = ambulance.entityInstancesProperties;
                 for (var i = 0; i < properties.length; i++) {
