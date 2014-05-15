044a352df0cd6734cf4a7265c9b5a43f5309ba68
Manuel Warum <manuel.warum.fl@ait.ac.at>
2014-05-15 11:21:36 +0200

    tabswitch: new operator that automagically switches tabs

---
+++ tabswitch/js/main.js
@@ -0,0 +1,35 @@
+/*global MashupPlatform,Wirecloud*/
+if (typeof MashupPlatform !== 'undefined') {
+    MashupPlatform.wiring.registerCallback('signal', function () {
+        var wirecloud = top.Wirecloud;
+        var id = MashupPlatform.prefs.get('tab_id');
+        console.log('Trying to switch to tab with id: ' + id);
+        var tab = null;
+
+        if (/[0-9]+/.test(id)) {
+            id = parseInt(id);
+            tab = wirecloud.activeWorkspace.tabInstances[id];
+        } else if(id.length > 0) {
+            var availableTabs = wirecloud.activeWorkspace.tabInstances;
+            for (var key in availableTabs) {
+                var thisTab = availableTabs[key];
+                if (thisTab.hasOwnProperty('nameText') && thisTab.nameText == id) {
+                    tab = thisTab;
+                    break;
+                }
+            }
+        }
+
+        if (tab)
+            wirecloud.activeWorkspace.notebook.goToTab(tab);
+        else
+            throw new NoSuchTabError('No tab by the name or identifier "' + id + '" could be found.');
+    });
+}
+
+function NoSuchTabError(message) {
+    this.name = 'NoSuchTabError';
+    this.message = message || 'No tab by that name was found';
+}
+
+NoSuchTabError.prototype = Object.create(Error.prototype);
