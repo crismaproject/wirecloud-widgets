4b78c1f13741af041b2551bca4e331460a4ba0d9
Manuel Warum <manuel.warum.fl@ait.ac.at>
2014-05-15 10:25:09 +0200

    wirecloud automation upload code (not quite working yet)

--- wirecloud.rb
+++ wirecloud.rb
@@ -18,12 +18,12 @@ class Wirecloud
     self.base_uri = base_uri
     self.agent = Mechanize.new do |config|
       config.follow_redirect = true
-      #config.log = Logger.new('log.txt')
+      config.log = Logger.new('log.txt')
     end
   end
 
   def login!(username, password)
-    agent.get(BASE_URI + LOGIN_PATH) do |login_page|
+    agent.get(base_uri + LOGIN_PATH) do |login_page|
       login_page.form_with(:class => 'formee') do |login_form|
         login_form['username'] = username
         login_form['password'] = password
@@ -34,14 +34,25 @@ class Wirecloud
     end
   end
 
+  def upload_widget(file)
+    agent.post(base_uri + RESOURCES_PATH, {
+        :force_create => true,
+        :file => File.new(file)
+    }) do |response|
+      PP.pp response
+      PP.pp response.body
+      PP.pp response.code
+    end
+  end
+
   def workspaces
-    agent.get(BASE_URI + WORKSPACES_PATH) do |workspaces|
+    agent.get(base_uri + WORKSPACES_PATH) do |workspaces|
       return JSON.parse workspaces.body unless workspaces.code != '200'
     end
   end
 
   def workspace(id)
-    agent.get(BASE_URI + WORKSPACES_PATH + "/#{id}") do |workspace|
+    agent.get(base_uri + WORKSPACES_PATH + "/#{id}") do |workspace|
       return JSON.parse workspace.body unless workspace.code != '200'
     end
   end
