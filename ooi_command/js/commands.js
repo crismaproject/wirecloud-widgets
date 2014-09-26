7d58207a1b5ebada3ad8f193e6a0a7acdac10bc3
Manuel Warum <manuel.warum.fl@ait.ac.at>
2014-09-26 11:25:10 +0200

    ooi_gis_map: integrating v2-mocks branch into v0.7 (UNTESTED)

--- ooi_command/js/commands.js
+++ ooi_command/js/commands.js
@@ -3,43 +3,27 @@ angular.module('ooiCommand.commands', [])
         {
             id: 'dispatch-from-station',
             css: 'ico-cmd-treat',
-            entityTypeId: 8,
             displayName: 'Move ambulances',
             help: 'This will dispatch a number of ambulances to the scene to perform an action.',
-            log: 'Move to lat. #{data.lat}, long. #{data.lon}',
+            log: 'Dispatching #{data[1]} from #{data[1]} to #{data[2]}, then #{data[3]}',
 
             arguments: [
+                {
+                    displayName: 'From',
+                    targetType: 'option',
+                    options: [ 'Closest to scene' ],
+                    getOptions: function (oois) {
+                        return [ 'Closest to scene' ]
+                            .concat(oois
+                                .filter(function (x) { return x.entityTypeId == 8 })
+                                .map(function (x) { return x.entityName })
+                        );
+                    }
+                },
                 {
                     displayName: 'How many',
                     targetType: 'number'
                 },
-                {
-                    displayName: 'From',
-                    targetType: 'option',
-                    options: [ 'Expel from station', 'Closest to scene' ]
-                },
-                {
-                    displayName: 'Send to',
-                    targetType: 'point',
-                    targetRestrictedTo: 14
-                },
-                {
-                    displayName: 'Then',
-                    targetType: 'option',
-                    options: [ 'Do nothing', 'Rescue', 'Treat', 'Transport', 'Refill' ]
-                }
-            ]
-        },
-
-        {
-            id: 'dispatch-amb',
-            css: 'ico-cmd-treat',
-            entityTypeId: 7,
-            displayName: 'Move ambulance',
-            help: 'This will dispatch the selected ambulance(s) to the scene to perform an action.',
-            log: 'Move to lat. #{data.lat}, long. #{data.lon}',
-
-            arguments: [
                 {
                     displayName: 'Send to',
                     targetType: 'ooi',
@@ -48,17 +32,8 @@ angular.module('ooiCommand.commands', [])
                 {
                     displayName: 'Then',
                     targetType: 'option',
-                    options: [ 'Do nothing', 'Rescue', 'Treat', 'Transport', 'Refill' ]
+                    options: [ 'do nothing', 'rescue', 'treat' ]
                 }
             ]
-        },
-
-        {
-            id: 'cancel',
-            css: 'ico-cmd-goto',
-            entityTypeId: 7,
-            displayName: 'Return',
-            help: 'Return to the station immediately.',
-            log: 'Return to station'
         }
     ]);
