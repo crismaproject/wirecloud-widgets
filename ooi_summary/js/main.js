f5ad20c12ddaad7e52bd18399c251a288eaba1cb
Manuel Warum <manuel.warum.fl@ait.ac.at>
2015-02-06 12:53:41 +0100

    ooi_summary: show actual patient data

--- ooi_summary/js/main.js
+++ ooi_summary/js/main.js
@@ -12,10 +12,12 @@ angular.module('ooiSummary', ['ooiSummary.wirecloud'])
          ****************************************************************/
         wirecloud.on('oois', function(oois) {
             oois = JSON.parse(oois);
+            var nameMap = {};
+            oois.forEach(function (x) {
+                nameMap[x.entityId] = x.entityName;
+            });
 
-            $scope.areas = oois
-                .filter(function (x) { return x.entityTypeId == 11 || x.entityTypeId == 8 })
-                .map(function (x) { return { name: x.entityName, green: 0, yellow: 1, red: 2 } });
+            countPatientStatus(oois, nameMap);
 
             $scope.stations = oois
                 .filter(function (x) { return x.entityTypeId == 8 })
@@ -23,4 +25,45 @@ angular.module('ooiSummary', ['ooiSummary.wirecloud'])
 
             $scope.$apply();
         });
+
+        function countResourceUsage(oois, ooiNameMap) {
+            var stationResourceMap = {}; // map station ID -> resource status
+            // TODO: pending server-side implementation
+        }
+
+        function countPatientStatus(oois, ooiNameMap) {
+            var areaPatientsMap = {}; // map area ID -> patient status
+            oois
+                .filter(function (x) { return x.entityTypeId == 10; })
+                .forEach(function (x) {
+                    var areaId = null;
+                    var life = null;
+                    x.entityInstancesProperties.forEach(function (y) {
+                        switch (y.entityTypePropertyId) {
+                            case 42:// patient prop ID 42 is life%
+                                life = parseInt(y.entityPropertyValue);
+                                break;
+                            case 45:// patient prop ID 45 is place id
+                                areaId = parseInt(y.entityPropertyValue);
+                                break;
+                        }
+                    });
+
+                    if (!areaPatientsMap.hasOwnProperty(areaId))
+                        areaPatientsMap[areaId] = { green: 0, yellow: 0, red: 0 };
+                    if (life <= 33)
+                        areaPatientsMap[areaId]['red']++;
+                    else if (life <= 66)
+                        areaPatientsMap[areaId]['yellow']++;
+                    else
+                        areaPatientsMap[areaId]['green']++;
+                });
+
+            var areas = [];
+            for (var areaId in areaPatientsMap) {
+                var value = areaPatientsMap[areaId];
+                areas.push({name: ooiNameMap[areaId], green: value.green, yellow: value.yellow, red: value.red});
+            }
+            $scope.areas = areas;
+        }
     }]);
