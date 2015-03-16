37c8a5961a98d6ea45ed15495a34985e429875cb
Manuel Warum <manuel.warum.fl@ait.ac.at>
2015-03-16 10:39:44 +0100

    ooi_info: additional API documentation

--- ooi_info/js/prettify.js
+++ ooi_info/js/prettify.js
@@ -1,5 +1,10 @@
 angular.module('ooiInfo.prettify', [])
     .factory('prettify', function () {
+        /**
+         * This is a class that encapsulates a rule that determines how an entity should be labeled. It is composed of
+         * id-based preconditions and a toString function.
+         * @constructor
+         */
         var Rule = function () {
             this.entityIds = [];
             this.propertyIds = [];
@@ -8,6 +13,7 @@ angular.module('ooiInfo.prettify', [])
         };
 
         /**
+         * Determines if this rule can be applied to an object instance with the specified ids.
          * @param {int} entityTypeId
          * @param {int} propertyId
          * @return {boolean}
@@ -17,44 +23,61 @@ angular.module('ooiInfo.prettify', [])
         };
 
         /**
+         * Generates a string representation of the object this rule applies to.
          * @param {int|String} propertyValue
          * @param {int} entityTypeId
          * @param {int} propertyId
-         * @return {*}
+         * @return {String}
          */
         Rule.prototype.toString = function (propertyValue, entityTypeId, propertyId) {
             return this.toStringCallback.call(this, propertyValue, entityTypeId, propertyId);
         };
 
+        /**
+         * Generates a string representation of the object this rule applies to in the context of tooltips.
+         * @param {int|String} propertyValue
+         * @param {int} entityTypeId
+         * @param {int} propertyId
+         * @return {String}
+         */
         Rule.prototype.toTitle = function (propertyValue, entityTypeId, propertyId) {
             return this.titleCallback.call(this, propertyValue, entityTypeId, propertyId);
         };
 
         /**
+	 * Sets for which types of entities this rules applies to.
          * @param {int} entityTypeId
          * @return {Rule}
          */
         Rule.prototype.forEntityType = function (entityTypeId) { this.entityIds.push(entityTypeId); return this; };
 
         /**
+	 * Sets for which properties this rule applies to.
          * @param {int} propertyId
          * @return {Rule}
          */
         Rule.prototype.forProperty = function (propertyId) { this.propertyIds.push(propertyId); return this; };
 
         /**
+	 * Sets the callback that generates a string representation of object instances satisfying the preconditions.
          * @param {Function} callback
          * @return {Rule}
          */
         Rule.prototype.thenDo = function (callback) { this.toStringCallback = callback; return this; };
 
-        Rule.prototype.useTitle = function (callback) { this.titleCallback = callback; return this; }
+        /**
+	 * Sets the callback that generates the title string representation of object instances satisfying the preconditions.
+         * @param {Function} callback
+         * @return {Rule}
+         */
+        Rule.prototype.useTitle = function (callback) { this.titleCallback = callback; return this; };
 
         var Prettifier = function () {
             this.rules = [];
         };
 
         /**
+	 * Creates a new, blank rule.
          * @return {Rule}
          */
         Prettifier.prototype.rule = function () {
@@ -64,6 +87,7 @@ angular.module('ooiInfo.prettify', [])
         };
 
         /**
+	 * Generates a string representation of the object instance using the first rule matching its preconditions. If no rule applies, the propertyValue will be returned verbatim.
          * @param {int|String} propertyValue
          * @param {int} entityTypeId
          * @param {int} propertyId
