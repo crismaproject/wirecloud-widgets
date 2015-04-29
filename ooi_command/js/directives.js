a289c5901526e706abfaac673e8198955812224e
Manuel Warum <manuel.warum.fl@ait.ac.at>
2015-04-29 11:35:22 +0200

    ooi_command: presentation of data

--- ooi_command/js/directives.js
+++ ooi_command/js/directives.js
@@ -14,12 +14,22 @@ angular.module('ooiCommand.directives', [])
                 $scope.updateSelected = function () {
                     $scope.selected = $scope.items
                         .filter(function (x) { return x.hasOwnProperty('$selected') && x['$selected']; })
-                        ;
+                    ;
                 };
 
                 $scope.stationNameFor = function (ooi) {
                     var rescueStation = ooiProperty(ooi, 313);
                     return rescueStation ? nameProvider.get(rescueStation.entityPropertyValue) : null;
+                };
+
+                $scope.activityFor = function (ooi) {
+                    var property = ooiProperty(ooi, 327);
+                    return property == null || !property.entityPropertyValue ? 'Idle' : property.entityPropertyValue;
+                };
+
+                $scope.cssClassFor = function (ooi) {
+                    var property = ooiProperty(ooi, 327);
+                    return property == null || !property.entityPropertyValue ? 'idle' : property.entityPropertyValue.toLowerCase();
                 };
             } ]
         }
